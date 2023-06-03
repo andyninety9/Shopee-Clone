0ab838b4871bed9a33f8c5fd74743683c3a75e3f
Mai Anh Duy <duy.maianh26@gmail.com>
2023-06-04 01:13:21 +0700

    Asidebar

--- src/pages/ProductList/AsideFilter/AsideFilter.tsx
+++ src/pages/ProductList/AsideFilter/AsideFilter.tsx
@@ -1,5 +1,5 @@
 import TocIcon from '@mui/icons-material/Toc'
-import { Link } from 'react-router-dom'
+import { Link, createSearchParams } from 'react-router-dom'
 import path from 'src/constants/path'
 import ArrowRightIcon from '@mui/icons-material/ArrowRight'
 import { orange } from '@mui/material/colors'
@@ -8,11 +8,26 @@ import Input from 'src/components/Input'
 import Button from 'src/components/Button'
 import StarOutlinedIcon from '@mui/icons-material/StarOutlined'
 import StarOutlineOutlinedIcon from '@mui/icons-material/StarOutlineOutlined'
+import { QueryConfig } from '../ProductList'
+import { Category } from 'src/types/category.type'
+import classNames from 'classnames'
 
-export default function AsideFilter() {
+interface Props {
+  queryConfig: QueryConfig
+  categories: Category[]
+}
+
+export default function AsideFilter({ queryConfig, categories }: Props) {
+  const { category } = queryConfig
+  console.log(category, categories)
   return (
     <div className='py-4'>
-      <Link to={path.home} className='flex items-center font-bold'>
+      <Link
+        to={path.home}
+        className={classNames('flex items-center font-bold', {
+          'text-orange': !category
+        })}
+      >
         <TocIcon
           sx={{
             width: '23px',
@@ -24,30 +39,38 @@ export default function AsideFilter() {
       </Link>
       <div className='my-4 h-[1px] bg-gray-300'></div>
       <ul>
-        <li className='py-2 pl-2'>
-          <Link to={path.home} className='relative px-2 text-[14px] font-semibold text-orange'>
-            <ArrowRightIcon
-              sx={{
-                fontSize: '20px',
-                color: orange[900],
-                position: 'absolute',
-                top: '1',
-                left: '-10px'
-              }}
-            />
-            Thời trang nam
-          </Link>
-        </li>
-        <li className='py-2 pl-2'>
-          <Link to={path.home} className='relative px-2 text-[14px] font-medium '>
-            Áo khoác
-          </Link>
-        </li>
-        <li className='py-2 pl-2'>
-          <Link to={path.home} className='relative px-2 text-[14px] font-medium'>
-            Áo vest
-          </Link>
-        </li>
+        {categories.map((categoryItem) => {
+          const isActive = category === categoryItem._id
+          return (
+            <li className='py-2 pl-2' key={categoryItem._id}>
+              <Link
+                to={{
+                  pathname: path.home,
+                  search: createSearchParams({
+                    ...queryConfig,
+                    category: categoryItem._id
+                  }).toString()
+                }}
+                className={classNames('relative items-center px-2 text-[14px] font-medium', {
+                  'items-center font-semibold text-orange': isActive
+                })}
+              >
+                {isActive && (
+                  <ArrowRightIcon
+                    sx={{
+                      fontSize: '20px',
+                      color: orange[900],
+                      position: 'absolute',
+                      top: '1',
+                      left: '-10px'
+                    }}
+                  />
+                )}
+                {categoryItem.name}
+              </Link>
+            </li>
+          )
+        })}
       </ul>
       <Link to={path.home} className='mt-4 flex items-center text-[14px] font-semibold uppercase'>
         <FilterAltOutlinedIcon
